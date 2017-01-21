c0f55b9e21cf28231142b61662233ccb19938b2d
ariashii <jarviswong64@gmail.com>
2017-01-21 00:51:03 -0800

    Tiles!
    No more green boxes
    actual sprite tiles (although it looks like shit)

--- THE GAME/Outside/Assets/Scripts/Level.cs
+++ THE GAME/Outside/Assets/Scripts/Level.cs
@@ -21,7 +21,18 @@ public class Level : MonoBehaviour {
 		for (int i = 0; i < levelWidth; i++) {
 			for (int j = 0; j < levelHeight; j++) {
 				if (levelDesign [i, j] == 0) {
-					Instantiate (ground, new Vector3 (gridScale * (i - levelWidth / 2), gridScale * (j - levelHeight / 2), -1), Quaternion.Euler (new Vector3 (0, 0, 0)));
+					GameObject currentTile = (GameObject)Instantiate (ground, new Vector3 (gridScale * (i - levelWidth / 2), gridScale * (j - levelHeight / 2), -1), Quaternion.Euler (new Vector3 (0, 0, 0)));
+					if (i == 0 || i == levelWidth - 1 || j == 0 || j == levelHeight - 1) {
+						currentTile.GetComponent<SpriteRenderer> ().sprite = currentTile.GetComponent<SpriteControl> ().ground2;
+					} else {
+						if (levelDesign [i, j + 1] == 1) {
+							currentTile.GetComponent<SpriteRenderer> ().sprite = currentTile.GetComponent<SpriteControl> ().ground1;
+						} else if (levelDesign [i, j - 1] == 1) {
+							currentTile.GetComponent<SpriteRenderer> ().sprite = currentTile.GetComponent<SpriteControl> ().ground3;
+						} else {
+							currentTile.GetComponent<SpriteRenderer> ().sprite = currentTile.GetComponent<SpriteControl> ().ground2;
+						}
+					}
 				}
 			}
 		}
